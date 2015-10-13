64e160ff98cde9f00ba04b2965245c1a31106877
ErikR <erik.ronstrom@doremir.com>
2015-10-13 14:03:14 +0200

    Added node-json-minify

--- repl.js
+++ repl.js
@@ -1,6 +1,7 @@
 var repl = require("repl").start({prompt: "> ", useGlobal: true});
 repl.context["teoria"] = require("teoria");
 repl.context["remus"] = require("./index");
+repl.context["JSON"].minify = require("node-json-minify");
 
 require('repl.history')(repl, process.env.HOME + '/.node_history');
 
